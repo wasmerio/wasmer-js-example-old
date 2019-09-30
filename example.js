bb9d3dca97ec9d5d8199f13e3d9caf3dc5413c64
Syrus <me@syrusakbary.com>
2019-09-30 16:11:57 -0700

    Improved example

--- example.js
+++ example.js
@@ -4,14 +4,12 @@ const WASI = require("@wasmer/wasi");
 const file = "./cowsay.wasm";
 
 // Instantiate a new WASI Instance
-let wasi = new WASI({ args: ["cowsay", "a"], env: {} });
+let wasi = new WASI({ args: ["cowsay", "Hello World"], env: {} });
 
 // Instantiating the WebAssembly file
 const wasm_bytes = new Uint8Array(fs.readFileSync(file)).buffer;
 WebAssembly.instantiate(wasm_bytes, {
   wasi_unstable: wasi.wasiImport,
 }).then(wasm => {
-    console.log("STARTING");
   wasi.start(wasm.instance);
-  console.log("ENDING");
 });
