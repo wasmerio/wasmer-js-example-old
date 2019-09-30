b8aa143dbce9c1419160b409cb65ab3048386746
Syrus <me@syrusakbary.com>
2019-09-30 16:05:24 -0700

    First version of wasmer-js

---
+++ example.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const WASI = require("@wasmer/wasi");
+
+const file = "./cowsay.wasm";
+
+// Instantiate a new WASI Instance
+let wasi = new WASI({ args: ["a"], env: {} });
+
+// Instantiating the WebAssembly file
+const wasm_bytes = new Uint8Array(fs.readFileSync(file)).buffer;
+WebAssembly.instantiate(wasm_bytes, {
+  wasi_unstable: wasi.wasiImport,
+}).then(wasm => {
+    console.log("STARTING");
+  wasi.start(wasm.instance);
+  console.log("ENDING");
+});
